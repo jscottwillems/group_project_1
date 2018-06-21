a75582e1630ed02c6b92ea2e291e29121ffe597d
Iverson <prestonedwards3@mac.com>
2018-06-20 21:37:18 -0500

    nothing

--- googleAPI.js
+++ googleAPI.js
@@ -11,12 +11,14 @@ console.log("linked")
 //place details request using the place_id
 //place details url is of the following form:
 var newURL = "https://maps.googleapis.com/maps/api/place/details/json?placeid=" + place_id + "&key=AIzaSyCVlepkh__TW03V4Vx1kOnXrdgQ61CIwZo"
-"https://maps.googleapis.com/maps/api/place/details/json?placeid=ChIJAYWNSLS4QIYROwVl894CDco&key=AIzaSyCVlepkh__TW03V4Vx1kOnXrdgQ61CIwZo"
+
 
 
 //place photos request
-var photoReference //returned from either a place search or a place details request
+var photoReference; //returned from either a place search or a place details request
 var maxHeight;
 var maxWidth;
 //place photos url is of the following form:
-var photosURL = "https://maps.googleapis.com/maps/api/place/photo?parameters"
+var photosURL = "https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=" + photoReference + "&key=AIzaSyCVlepkh__TW03V4Vx1kOnXrdgQ61CIwZo"
+
+var cityName = response.result.address_components[0].long_name
